8a2885c8b7943dd35eeaf8dc8da5b4e761dad917
BearAndBunny <koppelhuber@gmail.com>
2026-05-02 03:56:59 +0200

    unsaved

--- flutter_easy_seo/flutter_easy_seo/lib/src/widgets/wrappers/seo_text_wrapper.dart
+++ flutter_easy_seo/flutter_easy_seo/lib/src/widgets/wrappers/seo_text_wrapper.dart
@@ -3,14 +3,8 @@ part of 'package:flutter_easy_seo/flutter_easy_seo.dart';
 enum SEOTextType { h1, h2, h3, h4, h5, h6, p }
 
 class SEOTextWrapper extends BaseSEOWrapper {
-  const SEOTextWrapper({
-    super.key,
-    required super.child,
-    this.textType = SEOTextType.p,
-    super.className,
-    super.attributes,
-    this.text,
-  });
+  const SEOTextWrapper(
+      {super.key, required super.child, this.textType = SEOTextType.p, super.className, super.attributes, this.text});
 
   final SEOTextType textType;
   final String? text;
